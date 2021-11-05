fd8d174eb5394b6ea8412e24a07f02c7b8d8a43e
Jonathan Adamson <jadamson382@gmail.com>
2021-11-05 13:31:13 -0500

    flat bases Stack

---
+++ pages/pilas.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+
+const Pilas = () => {
+  const [stack, setStack] = useState([]);
+  const [cima, setCima] = useState(stack);
+
+  const handleAdd = () => {
+    setStack([...stack, Math.floor(Math.random() * 99)]);
+  };
+
+  const handleRemove = () => {
+    setStack(stack.slice(0, stack.length - 1, ...stack));
+  };
+
+  const getLastElement = () => {
+    setCima(stack.slice(-1)[0]);
+    console.log(stack);
+  };
+
+  useEffect(() => {
+    getLastElement();
+  });
+  const encabezado = <h1>Estructura de datos Pilas</h1>;
+  return (
+    <div className={styles.grid}>
+      {encabezado}
+      <div className={styles.grid}>
+        <a href="/" className={styles.card}>
+          <h2>Home</h2>
+        </a>
+        <button className={styles.card} onClick={handleAdd}>
+          Agregar elemento
+        </button>
+        <button className={styles.card} onClick={handleRemove}>
+          Retirar elemento
+        </button>
+      </div>
+      <h4>valor de cima = {cima}</h4>
+      <ul>
+        <div>
+          {stack.map((s, i) => (
+            <li key={i}>
+              indice: {i}, valor: {s}
+            </li>
+          ))}
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default Pilas;
